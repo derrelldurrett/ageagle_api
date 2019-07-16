2f0d93e5586a9d8700f937d88468967ead820382
derrelldurrett <derrelldurrett+github@gmail.com>
2019-07-16 14:48:10 -0600

    With working cucumber tests

--- features/step_definitions/display_weather_steps.rb
+++ features/step_definitions/display_weather_steps.rb
@@ -6,9 +6,10 @@ end
 When 'I enter {int} points to display' do |n_points|
   fill_in 'enter-points', with: n_points
   click_button 'See Weather'
+  sleep 5
 end
 
 Then 'I expect to see {int} points with temperatures' do |n_points|
-  temps = page.evaluate_script("window.map.getSource('temps')._data")
+  temps = page.evaluate_script("window.map.getSource('temps')._data.features")
   expect(temps.length).to be(5) #
 end
